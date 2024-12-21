ee9ffbff48bc795e248fa99dabdda3f1736453de
aththaariq001 <66201084+aththaariq001@users.noreply.github.com>
2024-12-21 21:34:06 +0800

    Delete Weekly_mgmt_report directory

--- Weekly_mgmt_report/components/graphs.py
+++
@@ -1,5 +0,0 @@
-from dash import dcc
-import plotly.graph_objects as go
-
-def create_graph(graph_id, title=""):
-    return dcc.Graph(id=graph_id, figure=go.Figure(layout=go.Layout(title=title, template="custom_flatly")))
